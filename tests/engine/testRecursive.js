45300ead356fc481c51388554cb6e33416f94a1d
NissimHadar <nissim.hadar@gmail.com>
2019-03-12 09:13:54 -0700

    Removed reticle test

--- tests/engine/testRecursive.js
+++ tests/engine/testRecursive.js
@@ -1,5 +1,5 @@
 // This is an automatically generated file, created by nitpick
-PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/NissimHadar/hifi_tests/addGridTests/tests/utils/branchUtils.js";
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 
 if (typeof depth === 'undefined') {
@@ -9,16 +9,11 @@ if (typeof depth === 'undefined') {
 
    nitpick.enableRecursive();
    nitpick.enableAuto();
-
-   if (typeof Test !== 'undefined') {
-       Test.wait(10000);
-   }
 } else {
    depth++
 }
 
 Script.include(testsRootPath + "engine/render/testRecursive.js");
-Script.include(testsRootPath + "engine/controller/testRecursive.js");
 
 if (depth > 0) {
    depth--;
--- tests/testRecursive.js
+++ tests/testRecursive.js
@@ -1,5 +1,5 @@
 // This is an automatically generated file, created by nitpick
-PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/NissimHadar/hifi_tests/addGridTests/tests/utils/branchUtils.js";
 Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
 
 if (typeof depth === 'undefined') {
@@ -9,10 +9,6 @@ if (typeof depth === 'undefined') {
 
    nitpick.enableRecursive();
    nitpick.enableAuto();
-
-   if (typeof Test !== 'undefined') {
-       Test.wait(10000);
-   }
 } else {
    depth++
 }
