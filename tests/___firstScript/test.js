56b1350e94ed43944e6831e15a2d036ad70ea291
NissimHadar <nissim.hadar@gmail.com>
2019-03-13 13:07:33 -0700

    Set snapshot location

---
+++ tests/___firstScript/test.js
@@ -0,0 +1,15 @@
+if (typeof PATH_TO_THE_REPO_PATH_UTILS_FILE === 'undefined') PATH_TO_THE_REPO_PATH_UTILS_FILE = "https://raw.githubusercontent.com/highfidelity/hifi_tests/master/tests/utils/branchUtils.js";
+Script.include(PATH_TO_THE_REPO_PATH_UTILS_FILE);
+var nitpick = createNitpick(Script.resolvePath("."));
+
+nitpick.perform("Initial setup", Script.resolvePath("."), "primary", function(testType) {
+    nitpick.addStep("Set snapshot location folder", function () {
+        Snapshot.setSnapshotsLocation("/sdcard/snapshot");
+    });
+
+    nitpick.addStep("Goto test domain", function () {
+        Window.location = "hifi://10.1.122.7/0,0,20";
+    });
+
+    nitpick.runTest(testType);
+});
